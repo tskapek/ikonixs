4077b8f6532fd15676ea722267f1511d59561352
tskapek <tskapek@gmail.com>
2021-06-29 15:44:34 -0400

    test jsdelivery

--- test.js
+++ test.js
@@ -1,31 +1,33 @@
 // ikonixs custom storefront
 
-// Load external script with store
-function loadConfig(ecwidStoreId, callback) {
-    var script = document.createElement("script");
-    script.setAttribute("src", '//example.com/myapp/store'+ecwidStoreId+'.js');
-    script.charset = "utf-8";
-    script.setAttribute("type", "text/javascript");
-    script.onreadystatechange = script.onload = callback;
-    document.body.appendChild(script);
-}
+// // Load external script with store
+// function loadConfig(ecwidStoreId, callback) {
+//     var script = document.createElement("script");
+//     script.setAttribute("src", '//example.com/myapp/store'+ecwidStoreId+'.js');
+//     script.charset = "utf-8";
+//     script.setAttribute("type", "text/javascript");
+//     script.onreadystatechange = script.onload = callback;
+//     document.body.appendChild(script);
+// }
 
-// Application functionality
-function adjustProductOptions() {
-    // do logic to properly show product options...
-}
+// // Application functionality
+// function adjustProductOptions() {
+//     // do logic to properly show product options...
+// }
 
-// Get color value for the message and store it in color variable
-var color = Ecwid.getAppPublicConfig(appId);
+// // Get color value for the message and store it in color variable
+// var color = Ecwid.getAppPublicConfig(appId);
 
-// Get Ecwid store ID and start working
-loadConfig(Ecwid.getOwnerId(), adjustProductOptions);
+// // Get Ecwid store ID and start working
+// loadConfig(Ecwid.getOwnerId(), adjustProductOptions);
 
-// When a product page is loaded
-    // display base options
+// // When a product page is loaded
+//     // display base options
 
-// When an option changes, recalculate which other options should be shown
+// // When an option changes, recalculate which other options should be shown
 
-Ecwid.OnProductOptionsChanged.add(function(productid) {
-    window.alert("Options, changed, product id: " + productid);
-})
+// Ecwid.OnProductOptionsChanged.add(function(productid) {
+//     window.alert("Options, changed, product id: " + productid);
+// })
+
+console.log("Hello world from github!")
